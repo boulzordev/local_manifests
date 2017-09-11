3b39a02e3297c82fb5c62d9cdd41a7f91059dcfd
Shreps <shr3ps@gmail.com>
2017-09-11 14:21:33 +0200

    Update repopick

--- repopicker.sh
+++ repopicker.sh
@@ -1,20 +1,40 @@
-repopick 185798
-repopick 185639
+# Already Merged
+#repopick 185798
+# 185521
+#repopick 185783;
+# 186684
+#repopick 185854 185855 185856 186576;
+#repopick 186670 186672 186673;
+#repopick 185929 186712;
+#repopick 185801
 
-#vendor/lineage
-repopick 188278 185870 188401 185852 185869 185491 185521 186505 187951;
+# Abandonned
+#repopick 185639
+# 185852 185869 186505
+
+
+#vendor/lineage + build
+repopick 188278 185870 188401 185491;
+#sepolicy
 repopick 186243 186244 186245 186246;
-repopick 185783;
-repopick 185642 185654 185672 185653 185656 185657 185658 185659 185888 186684;
+
+# charger/healthd
+repopick 185642 185654 185672 185653 185656 
+repopick 185657 185658 185659 185888
+
+# hardware/qcom/gps
 repopick 186455 186900 186901;
-repopick 185854 185855 185856 186576;
-repopick 186670 186672 186673;
-repopick 185929 186712;
+
+# Toybox
 repopick 186302;
+# build
 repopick 186740;
+# releasetool
 repopick 186687;
-repopick 185636 185639 185640;
-repopick 185801
+
+# alarm - bandonned 185639
+# Bionic
+repopick 185636 185640;
 
 #build/soong
 repopick 188279 188280
@@ -30,3 +50,6 @@ repopick 188685 188682
 
 #hardware/qcom/media-caf/msm8916
 repopick 188686
+
+# vendor/lineage healthd - Might conflict
+repopick 187951;
